b8f4d14627280bf281410ee5afdafb0a0a11ad66
tadvepio <32989919+tadvepio@users.noreply.github.com>
2021-12-19 00:44:46 +0200

    2/3 done from tasks

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -2,7 +2,7 @@ fetch = require('cross-fetch')
 
 module.exports = {
     index: (req,res) => {
-        res.render("index", {labels: res.locals.x, values: res.locals.y})
+        res.render("index", {labels: res.locals.x, values: res.locals.y, highest: Math.max(...res.locals.y), highestDate: res.locals.yDate, bearings: res.locals.bearings})
     },
     formatForChart: (req, res, next) => {
         let prices = res.locals.data.prices
@@ -28,12 +28,35 @@ module.exports = {
     calcBearAndHigh: (req, res, next) => {
         let x = res.locals.x;
         let y = res.locals.y;
-        let yMax = Math.max(...y)
-        let maxLow = 0;
+        let count = 0;
+        let maxBearing = 0;
+        let bearStart = "";
+        let bearEnd = "";
+        let bearEndValue = 0;
+        let bearStartValue = 0;
         // calculate longest bearing
-        res.locals.yMax = yMax;
-        res.locals.lBear = maxLow;
-        console.log(yMax)
+        for (let i = 0; i < y.length; i++) {
+            if ( y[i] > y[i+1] ) {
+                count += 1
+            } else {
+                if (count > maxBearing) {
+                    maxBearing = count
+                    bearStart = x[i-count]
+                    bearEnd = x[i]
+                    bearStartValue = y[i-count]
+                    bearEndValue = y[i]
+                }
+                count = 0
+            }
+        }
+        let bearings = [maxBearing, bearStart, bearEnd, bearStartValue, bearEndValue]
+        res.locals.yMax = Math.max(...res.locals.y);
+        let timeStamp = parseInt(x[y.indexOf(Math.max(...res.locals.y))]);            
+        let toMill = timeStamp;
+        let dateObj = new Date(toMill)
+        let yDateString = dateObj.toLocaleDateString("en-US", {timeZone: 'UTC'})
+        res.locals.yDate = yDateString
+        res.locals.bearings = bearings;
         next()
     },
     getData: async (req, res, next) => {
