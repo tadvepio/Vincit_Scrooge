15078f72c3c02aebc92a729b69c688cd61e0d431
tadvepio <32989919+tadvepio@users.noreply.github.com>
2021-12-19 19:04:24 +0200

    invest working

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -2,7 +2,7 @@ fetch = require('cross-fetch')
 
 module.exports = {
     index: (req,res) => {
-        res.render("index", {labels: res.locals.x, values: res.locals.y, highest: Math.max(...res.locals.y), highestDate: res.locals.yDate, bearings: res.locals.bearings})
+        res.render("index", {labels: res.locals.x, values: res.locals.y, highest: Math.max(...res.locals.y), highestDate: res.locals.yDate, bearings: res.locals.bearings, investmentInfo: res.locals.investmentInfo})
     },
     formatForChart: (req, res, next) => {
         let prices = res.locals.data.prices
@@ -23,6 +23,43 @@ module.exports = {
         })
         res.locals.x = xLabels;
         res.locals.y = yValues;
+        next();
+    },
+    invest: (req, res, next) => {
+        let investDate = "";
+        let sellDate = "";
+        let profit = 0;
+        let profit2 = 0;
+        let investPrice = 0;
+        let sellPrice = 0;
+        let forComparisony = []
+
+        res.locals.y.forEach(item => {
+            forComparisony = res.locals.y.slice(res.locals.y.indexOf(item)+1)
+            profit2 = Math.max(...forComparisony)-item;
+            if (profit2 > profit) {
+                profit = profit2
+                investDate = res.locals.x[res.locals.y.indexOf(item)]
+                investPrice = item
+                sellDate = res.locals.x[res.locals.y.indexOf(Math.max(...forComparisony))]
+                sellPrice = Math.max(...forComparisony)
+            } else {
+                return
+            }
+        })
+
+        let toMill = parseInt(investDate)
+        let dateObj = new Date(toMill)
+        let y = dateObj.toLocaleDateString("en-US", {timeZone: 'UTC'})
+        investDate = y;
+
+        let toMill2 = parseInt(sellDate)
+        let dateObj2 = new Date(toMill2)
+        let y2 = dateObj2.toLocaleDateString("en-US", {timeZone: 'UTC'})
+        sellDate = y2
+
+        res.locals.investmentInfo = [profit, investDate, investPrice, sellDate, sellPrice]
+        console.log(res.locals.investmentInfo)
         next();
     },
     calcBearAndHigh: (req, res, next) => {
@@ -59,9 +96,10 @@ module.exports = {
         res.locals.bearings = bearings;
         next()
     },
-    getData: async (req, res, next) => {
+    getData: async (req, res, next) => { 
             let from = res.locals.from
-            let to = res.locals.to
+            let to = res.locals.to 
+        
         fetch(`https://api.coingecko.com/api/v3/coins/bitcoin/market_chart/range?vs_currency=eur&from=${from}&to=${to}`)
         .then(resp => {
             return resp.json()
